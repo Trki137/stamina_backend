aa9031a1221052a3e1c014c601ce60a0a16edbfe
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-09 11:44:43 +0100

    Created seed and one pool for db connection

---
+++ db/db.js
@@ -0,0 +1,18 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "Stamina",
+  password: "bazepodataka",
+  port: 5432,
+});
+
+module.exports = {
+  query: (text, params) => {
+    return pool.query(text, params).then((res) => {
+      return res;
+    });
+  },
+  pool: pool,
+};
