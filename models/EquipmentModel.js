f8d9341b6ebca4cc5e18a8e18841ddd3ae94cf25
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-24 20:13:50 +0100

    Route and model for equipment is finished

--- models/EquipmentModel.js
+++ models/EquipmentModel.js
@@ -1,5 +1,18 @@
 const db = require("../db/db.js");
 
 module.exports = class Equipment {
+  static async getAllEquipment() {
+    const query = `SELECT *
+                   FROM equipment`;
 
+    try {
+      const result = await db.query(query, []);
+
+      return result.rowCount > 0 ? result.rows : null;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+
+  }
 }
