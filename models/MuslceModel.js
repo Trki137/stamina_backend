7da1bf751d8222170eb74d098a35453f39847273
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-24 20:07:27 +0100

    Route and model for muscle are done

--- models/MuslceModel.js
+++ models/MuslceModel.js
@@ -2,4 +2,29 @@ const db = require("../db/db.js");
 
 module.exports = class Muscle {
 
+   static async getAllMuscles(){
+    const query = `SELECT * FROM muscle_group`;
+
+    try{
+      const result = await db.query(query,[]);
+
+      return result.rowCount !== 0 ? Muscle.renameMuscles(result.rows) : null;
+    }catch (e){
+      console.log(e);
+    }
+  }
+
+  static renameMuscles(data){
+    for(let i = 0;  i < data.length; i++){
+      data[i].input_name = data[i].name;
+      if(data[i].name.includes("-")){
+        data[i].name = data[i].name.replace("-", " ");
+      }
+
+      data[i].name = data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1);
+    }
+
+    return data;
+  }
+
 }
--- routes/muscle_group.routes.js
+++ routes/muscle_group.routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-router.get("all-muscles", (req,res,next) => {
+const Muscle = require("../models/MuslceModel")
+router.get("/all-muscles", (req,res,next) => {
   (async () => {
+      const result = await Muscle.getAllMuscles();
 
+      if(!result){
+        res.status(500);
+        res.send("Couldn't get data");
+        return;
+      }
+
+      res.send(result);
   })();
 });
 module.exports = router;
