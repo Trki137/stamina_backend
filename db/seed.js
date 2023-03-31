76243da510fe3da8f8bf883e2b8624d70e1d158f
Dean Trkulja <deantrkulja2001@gmail.com>
2023-03-31 23:07:07 +0200

    Fixing problems with db table training

--- db/seed.js
+++ db/seed.js
@@ -69,7 +69,10 @@ const sql_create_training_table = `
         intensity    VARCHAR(20) NOT NULL,
         description  VARCHAR(300),
         trainingId   SERIAL      NOT NULL,
-        avg_calories INT,
+        avg_calories NUMERIC(2),
+        numOfSets int NOT NULL,
+        restBetweenSets int NOT NULL,
+        restBetweenWorkouts int NOT NULL
         PRIMARY KEY (trainingId),
         UNIQUE (name)
     );
